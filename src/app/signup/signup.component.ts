ff35f29d5500bc7f18f2d65347b08ab2050a8b06
Huy <quanghuycsgl@gmail.com>
2023-05-07 22:01:00 +0700

    sign up

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl, Validators} from '@angular/forms'
-import { Router } from '@angular/router'
-
+import { AbstractControl, FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import Validation from '../types/validation';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -9,15 +8,63 @@ import { Router } from '@angular/router'
 })
 export class SignupComponent implements OnInit{
 
-  registerForm: FormGroup = new FormGroup({
-    name: new FormControl(null,[Validators.required]),
-    email: new FormControl(null,[Validators.email,Validators.required]),
-    password: new FormControl(null,[Validators.required]),
-    cpass: new FormControl(null,[Validators.required])
-  })
-  constructor() { }
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    acceptTerms: new FormControl(false),
+  });
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        username: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20)
+          ]
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40)
+          ]
+        ],
+        confirmPassword: ['', Validators.required],
+        acceptTerms: [false, Validators.requiredTrue]
+      },
+      {
+        validators: [Validation.match('password', 'confirmPassword')]
+      }
+    );
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.form.value, null, 2));
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
   }
 
 }
