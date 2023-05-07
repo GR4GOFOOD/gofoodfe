1efcd74bba31cce5824e19b53e4463e5e35b355d
Huy <quanghuycsgl@gmail.com>
2023-05-07 09:49:39 +0700

    fix bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
+// import { MatToolbarModule } from '@angular/material/toolbar';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatCardModule } from '@angular/material/card';
 import { RouterModule }  from '@angular/router'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -22,30 +22,24 @@ import { PaymentComponent } from './payment/payment.component';
 import { PaymentAtmComponent } from './payment-atm/payment-atm.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { FAQSComponent } from './faqs/faqs.component';
-<<<<<<< HEAD
 import { PremmiumComponent } from './premmium/premmium.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { CartComponent } from './cart/cart.component';
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-
-import { CartComponent } from './cart/cart.component';
-import { ListComponent } from './list/list.component';
-import { HttpClientModule } from '@angular/common/http';
 import { DetailComponent } from './detail/detail.component';
->>>>>>> bafa6078f040833fb5215756db8dedd4380339a4
+import { ListComponent } from './list/list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+
+
+
+
+
+
+
+
+
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -56,44 +50,30 @@ import { DetailComponent } from './detail/detail.component';
     SignInComponent,
     ContactComponent,
     MenuComponent,
-    // SignupComponent,
+    SignupComponent,
     BlogComponent,
     PaymentComponent,
     PaymentAtmComponent,
-    ProductDetailComponent,
     HomepageComponent,
     FAQSComponent,
-<<<<<<< HEAD
-    PremmiumComponent,
-    ProductDetailComponent,
-    CartComponent,
-
-
-
-
-=======
-
     CartComponent,
     ListComponent,
     DetailComponent,
->>>>>>> bafa6078f040833fb5215756db8dedd4380339a4
+    PremmiumComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatCardModule,
+    // MatToolbarModule,
+    // MatIconModule,
+    // MatButtonModule,
+    // MatCardModule,
     RouterModule,
     ReactiveFormsModule,
-=======
-    HttpClientModule
->>>>>>> bafa6078f040833fb5215756db8dedd4380339a4
   ],
   providers: [],
   bootstrap: [AppComponent]
