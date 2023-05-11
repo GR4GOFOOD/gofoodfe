baee99e5e2bef050d87fde9ff56d94da195efd6d
AnhhNgoccc <anhhngocc22@gmail.com>
2023-05-12 01:16:16 +0700

    update homepage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductAPIService } from '../service/product-api.service';
 
 @Component({
   selector: 'app-homepage',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent {
+  products: any;
+  bestproducts: any;
+  errMessage: string=''
+
+  constructor(private _service: ProductAPIService, private router: Router) {
+    this._service.getProducts().subscribe({
+      next:(data)=>{
+        this.products=data;
+
+        this.bestproducts = data.filter((product: any) => {
+          return product.productId >= 39 && product.productId <= 42
+        });},
+
+        error:(err)=>{this.errMessage=err}
+         })
+        }
+
+      //   onProductDetails(id: string){
+      //   this.router.navigate(['/product-detail', id]);
+      // }
+
+      onProductDetails(id: string) {
+        this.router.navigate(['/product-detail', id]);
+      }
 
 }
