fc1881f23a013f60dde6d879c5e43014a151d879
quynhlk <quynhptt20406@st.uel.edu.vn>
2023-05-12 10:56:15 +0700

    cartService

--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -63,7 +63,7 @@ getProductid(id:string | null):Observable<any>{
   updateCartItem(item: any): void {
         const index = this.cartItems.findIndex((cartItem: any) => cartItem.ID === item.ID);
       if (index !== -1) {
-      this.cartItems[index].QUANTITY = item.QUANTITY;
+      this.cartItems[index].productQty = item.productQty;
     }
   }
 
